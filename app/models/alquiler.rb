676dac6e1f97c74c9c1af7442be4de686f961ea4
lumir <lumir.olivares@koombea.com>
2012-11-17 11:49:08 -0500

    change table price

--- app/models/alquiler.rb
+++ app/models/alquiler.rb
@@ -2,7 +2,7 @@ class Alquiler < ActiveRecord::Base
    attr_accessible :name, :identifier, :address, :phone, :chairs, :tables, :big_tables, :tablecloth, :big_tablecloth, :deliver_date, :total, :abono, :status 
 
    AMMOUNT = {chairs: 630, tables: 38, big_tables: 64, tablecloth: 35, big_tablecloth: 19 } 
-   PRICES = {chairs: 500, tables: 800, big_tables: 2000, tablecloth: 2000, big_tablecloth: 3000 }
+   PRICES = {chairs: 500, tables: 1000, big_tables: 2000, tablecloth: 2000, big_tablecloth: 3000 }
 
    def self.get_available(date)
       available_chairs = AMMOUNT[:chairs] - self.sum(:chairs, conditions: {:deliver_date => date})      
